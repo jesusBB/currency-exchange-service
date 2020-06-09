bbefecc736e0a2e8431fdb048802c7a0b6a3ef29
Jesus Bustillo <jesus.bustillo@cdl.co.uk>
2020-06-09 15:23:10 +0100

    adding port

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,17 @@ public class CurrencyExchangeController {
     @Autowired
     private ExchangeValueRepository repository;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
-        return repository.findByFromAndTo(from, to);
+        ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+
+        exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 
         // return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
-
+        return exchangeValue;
     }
 }
--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -19,6 +19,8 @@ public class ExchangeValue {
     private String to;
     private BigDecimal conversionMultiple;
 
+    private int port;
+
     public ExchangeValue() {
     }
 
@@ -60,4 +62,13 @@ public class ExchangeValue {
     public void setConversionMultiple(BigDecimal conversionMultiple) {
         this.conversionMultiple = conversionMultiple;
     }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
 }
