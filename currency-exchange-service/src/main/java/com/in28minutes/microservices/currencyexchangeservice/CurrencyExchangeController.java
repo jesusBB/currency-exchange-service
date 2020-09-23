cec1ee989a5acf14103d9ae7b9f514482b1c6f59
Jesus Bustillo <jesus.bustillo@cdl.co.uk>
2020-09-23 12:03:55 +0100

    Sleuth and zipkin

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     private ExchangeValueRepository repository;
@@ -22,6 +26,7 @@ public class CurrencyExchangeController {
 
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 
+        logger.info("{}", exchangeValue);
         // return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
         return exchangeValue;
     }
